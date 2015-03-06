ba43901b0bff6fd0fb83eac7a383a3629d98b9c3
YusukeKokubo <yusuke.kokubo@gmail.com>
2015-03-06 23:53:42 +0900

    add button to click

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -4,6 +4,8 @@ import scala.scalajs.js.JSApp
 import org.scalajs.dom
 import dom.document
 
+import scala.scalajs.js.annotation.JSExport
+
 object TutorialApp extends JSApp {
 
   def main(): Unit = {
@@ -15,5 +17,10 @@ object TutorialApp extends JSApp {
     val textNode = document.createTextNode(text)
     parNode.appendChild(textNode)
     targetNode.appendChild(parNode)
+  }
+
+  @JSExport
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
   }
 }
