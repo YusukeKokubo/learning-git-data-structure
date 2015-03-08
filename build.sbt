e1ecaef7ddc32ca76b98f97c82b5ffcf4619f49b
YusukeKokubo <yusuke.kokubo@gmail.com>
2015-03-09 00:09:34 +0900

    use Scala.rx and Scalatags

--- build.sbt
+++ build.sbt
@@ -10,6 +10,8 @@ libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
 libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
 libraryDependencies += "com.lihaoyi" %%% "utest" % "0.3.0" % "test"
 libraryDependencies += "com.lihaoyi" %%% "upickle" % "0.2.6"
+libraryDependencies += "com.lihaoyi" %%% "scalatags" % "0.4.5"
+libraryDependencies += "com.lihaoyi" %%% "scalarx" % "0.2.7"
 
 skip in packageJSDependencies := false
 
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,31 +1,44 @@
 package tutorial.webapp
 
-import lib.{GitHub, Ajax}
+import lib.{Repository, GitHub}
+import org.scalajs.dom
+import rx.core.{Var, Rx}
 
-import scala.scalajs.js
-import scala.scalajs.js.{Dynamic, JSApp}
+import scala.scalajs.js.{JSApp}
 import org.scalajs.jquery.jQuery
 
 import scala.util.{Failure, Success}
 
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
+import scalatags.JsDom.tags2.section
+
+import scalatags.JsDom.all._
 
 object TutorialApp extends JSApp {
+  import Framework._
+
+  val repositories: Var[Seq[Repository]] = Var(Seq[Repository]())
 
   def main(): Unit = {
-    jQuery(setupUI _)
+    setupUI()
+    dom.document.body.appendChild(
+      section(
+        Rx {
+          ul(
+            for (r <- repositories()) yield {
+              li(
+                r.name
+              )
+            }
+          )
+        }).render
+    )
   }
 
   def addClickedMessage(): Unit = {
 
     GitHub.repos("YusukeKokubo").onComplete {
-      case Success(msg) => {
-        jQuery("body").append("<ul>")
-        msg.foreach(r =>
-          jQuery("body").append("<li>" + r.name + "</li>")
-        )
-        jQuery("body").append("</ul>")
-      }
+      case Success(msg) => repositories.update(msg)
       case Failure(t) => jQuery("body").append(t.getMessage)
     }
   }
